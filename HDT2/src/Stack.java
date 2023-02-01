35d3b72e99fecb75180a42b1d432954d62cf554e
chuy-zip <andreschuy03@gmail.com>
2023-02-01 10:05:01 -0600

    file can now be readed from main

--- HDT2/src/Stack.java
+++ HDT2/src/Stack.java
@@ -1,4 +1,28 @@
 
-public class Stack {
+public class Stack<T> implements IStack<T>{
+
+	@Override
+	public T pop() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void push() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public T Peek() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int Size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
--- HDT2/src/StackManager.java
+++ HDT2/src/StackManager.java
@@ -1,4 +1,34 @@
 
-public class StackManager {
+public class StackManager implements ICalculator{
+
+	@Override
+	public float Add(float number1, float number2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public float Substraction(float number1, float number2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public float Multiply(float number1, float number2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public float Divition(float number1, float number2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public float getNextInput() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
