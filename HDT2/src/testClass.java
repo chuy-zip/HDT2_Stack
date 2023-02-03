796ce2d19e28db3ec85a8aa29ce4f0b673166ae0
chuy-zip <andreschuy03@gmail.com>
2023-02-02 23:43:11 -0600

    Added Unitary test

--- HDT2/src/testClass.java
+++ HDT2/src/testClass.java
@@ -10,7 +10,7 @@ public class testClass {
 	public void popReturnsDeletedItemAtTheTopOfStack() {
 		Stack<Object> testStack = new Stack<Object>();
 		testStack.push(3.0f);
-		testStack.push(4);
+		testStack.push(4.0f);
 		testStack.push(2.0f);
 		testStack.push(1.0f);
 		
@@ -28,5 +28,48 @@ public class testClass {
 		testStack.pop();
 		
 		assertEquals( 2.0f, testStack.Peek());
+	}
+	
+	@Test
+	public void pushCanBeMadeWithDifferentDataTypeInt() {
+		Stack<Object> testStack = new Stack<Object>();
+		testStack.push(3);
+		testStack.push(4);
+		testStack.push(9);
+		testStack.push(0);
+		testStack.pop();
+		
+		assertEquals( 9, testStack.Peek());
+	}
+	
+	@Test
+	public void PushAddsNewItemToTheTheTopOfStack() {
+		Stack<Object> testStack = new Stack<Object>();
+		testStack.push(3.32);
+		testStack.pop();
+		testStack.push(4.00);
+		testStack.push(9.23);
+		testStack.pop();
+		testStack.pop();
+		testStack.push(0.24);
+		testStack.push(9.21);
+		testStack.push(9.91);
+		
+		assertEquals( 9.91, testStack.Peek());
+	}
+	
+	@Test
+	public void ResultaOfAditionIs15f() {
+		Stack<Object> testStack = new Stack<Object>();
+		StackManager OperationTester = new StackManager();
+		
+		testStack.push(3.0f);
+		testStack.push(4.0f);
+		testStack.push(9.0f);
+		testStack.push(6.0f);
+		float val1 = (float) testStack.pop();
+		float val2 = (float) testStack.pop();
+		
+		assertEquals( 15.0f , OperationTester.Add(val1, val2) , 0D);
 	}
 }
